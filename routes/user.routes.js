e66cb1f110acfed2fcd3b334a99444d55c14c649
Jo Huerta <santiago@alimx.mx>
2022-09-13 13:06:30 -0500

    se agrega barra de navegacion

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -4,13 +4,13 @@ const router = require("express").Router();
 // all your routes here
 
 //localhost:3000/user-profile
-router.get("/user-profile", (req, res) => {
-    res.render("users/user-profile")
+router.get("/user-profile", (req, res) => {    
+    res.render("users/user-profile",{correo:req.session.user.correo, nombre:req.session.user.nombre});
 });
 
 //localhost:3000/user-profile/new-event
 router.get("/new-event", (req, res) => {
-    res.render("users/user-new-event")
-})
+    res.render("users/user-new-event");
+});
 
 module.exports = router;
