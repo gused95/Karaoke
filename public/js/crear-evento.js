9c65a9677c4a166723f4707798344afc830841d5
Daniela Romero <danyrp45@gmail.com>
2022-09-15 19:25:41 -0500

    Edita eventos en el perfil del usuario

--- public/js/crear-evento.js
+++ public/js/crear-evento.js
@@ -1,49 +1,49 @@
 const crearEvento=()=>{
   const aux = document.querySelector('#form-evento')
   const inner= `
-  <form action="/user/new-event" method="post">
-  <div class="inicio">
-    <h1>Datos del evento.</h1>
-    <label for="">
-      Nombre del evento:
-      <input type="text" name="eventName" id="" class="control" />
-    </label>
-    <label for="">
-      Fecha del evento:
-      <input
-        type="datetime-local"
-        name="eventDate"
-        id=""
-        class="control"
-      />
-    </label>
-    <button type="submit">Crear Evento</button>
-  </div>
-</form>
+    <form action="/user/new-event" method="post">
+      <div class="inicio">
+        <h1>Datos del evento.</h1>
+        <label for="">
+          Nombre del evento:
+          <input type="text" name="eventName" id="" class="control" />
+        </label>
+        <label for="">
+          Fecha del evento:
+          <input
+            type="datetime-local"
+            name="eventDate"
+            id=""
+            class="control"
+          />
+        </label>
+        <button type="submit">Crear Evento</button>
+      </div>
+    </form>
   `
 
   aux.innerHTML = inner
 }
 
 const editarEvento=(name,fecha,id)=>{
-const fechaAux=new Date(fecha)
-console.log(fecha)
-  const aux=document.querySelector('#form-evento')
-  const inner=`<body class="">
-  <div class="fondo3">
-      <form action="/user/edit-event/${id}" method="post">
-          <div class="inicio">
-              <h1>Datos del evento.</h1>
-              <label for=""> Nombre del evento:
-                  <input type="text" name="eventName" id="" class='control' value="${name}">
-              </label>
-              <label for=""> Fecha del evento:
-                  <input type="datetime-local" name="eventDate" id="" class='control' value=${fecha}>
-              </label>
-              <button type="submit">Actualizar evento</button>
-          </div>
-      </form>
-  </div>    
-</body>`
-aux.innerHTML=inner
+  const aux= document.querySelector('#form-evento')
+  const inner=`
+    <div class="fondo3">
+        <form action="/user/edit-event/${id}" method="post">
+            <div class="inicio">
+                <h1>Datos del evento.</h1>
+                <label for=""> 
+                  Nombre del evento:
+                  <input type="text" name="eventName" class='control' value="${name}">
+                </label>
+                <label for=""> 
+                  Fecha del evento:
+                  <input type="datetime-local" name="eventDate" class='control' value="${fecha}">
+                </label>
+                <button type="submit">Actualizar evento</button>
+            </div>
+        </form>
+    </div>    
+  `
+  aux.innerHTML=inner
 }
